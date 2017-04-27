920e005bb05b2dbbf1d651fed075fac3d8497e92
julianvasquez <julian.vasquez@netuxtecnologia.com>
2017-04-27 08:42:17 -0500

    add child process (fork) example

--- demo.js
+++ demo.js
@@ -1,31 +1,9 @@
 'use strict'
 
 const addon = require('./build/Release/eas-rtsp')
-const fs = require('fs')
 
 
-addon.getBuffer((err, buffer) => {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log(buffer.toString('hex'))
-	console.log('*********Otro buffer...**********')
-	addon.getBuffer((err, buffer) => {
-	  if (err) {
-		console.log(err)
-	  } else {
-		console.log(buffer.toString('hex'))
-	  }
-	})
-	// fs.writeFile('stream.h264', buffer, 'binary', function(err) {
-	//   if(err) {
-    //     console.log(err)
-	//   } else {
-    //     console.log("The file was saved!")
-	//   }
-    // })
-  }
-})
+console.log(`native addon whoami: ${addon.getBuffer()}`)
 
 // Code not reached
 console.log('continue...')
--- src/eas-rtsp.cc
+++ src/eas-rtsp.cc
@@ -52,6 +52,7 @@ class NodeWorker : public AsyncWorker {
 		RTSPConnection rtspClient(env, &cb, "rtsp://admin:@192.168.1.38:554/live1.sdp", *m_node_buffer);
 		signal(SIGINT, sig_handler);
 		signal(SIGUSR1, sig_handler);
+
 		env.mainloop();
 	
 	}
@@ -72,10 +73,57 @@ class NodeWorker : public AsyncWorker {
 	char m_stop = 1;
 };
 
-NAN_METHOD(getBuffer) {
-	Nan::Callback *node_callback = new Nan::Callback(info[0].As<Function>());
+void hijo(void) {
+	std::vector<uint8_t> * m_node_buffer;
+	
+	class RTSPCallback : public RTSPConnection::Callback
+	{
+	  public:
+		virtual bool onData(const char* id, unsigned char* buffer, ssize_t size, struct timeval presentationTime, vector<unsigned char>& m_node_buffer) {
+			m_node_buffer.clear();
+			m_node_buffer.insert(m_node_buffer.end(), marker, marker+sizeof(marker));
+			m_node_buffer.insert(m_node_buffer.end(), buffer, buffer+size);
 
-	AsyncQueueWorker(new NodeWorker(node_callback));
+			//std::cout << id << " " << size << " ts:" << presentationTime.tv_sec << "." << presentationTime.tv_usec << std::endl;
+			return true;
+		}
+	};
+
+	// Reserva espacio en memoria para el puntero al vector<uint8_t>
+	m_node_buffer = new vector<uint8_t>();
+			
+	Environment env(stop);
+	RTSPCallback cb;
+	RTSPConnection rtspClient(env, &cb, "rtsp://admin:@192.168.1.38:554/live1.sdp", *m_node_buffer);
+	signal(SIGINT, sig_handler);
+	signal(SIGUSR1, sig_handler);
+
+	env.mainloop();
+}
+
+NAN_METHOD(getBuffer) {
+	// Nan::Callback *node_callback = new Nan::Callback(info[0].As<Function>());
+
+	pid_t pid;
+	int status;
+
+	pid=fork();
+	if (pid == -1) {
+		Nan::ThrowError("Can't create child process...");
+		return;
+	}
+
+	if (pid == 0) {
+        printf("*********** I am in the child with PID = %d *****************\n", childpid); 
+		hijo();
+    }
+
+	// Solo el padre, no es alcanzado por el hijo porque se queda en el event
+	// loop de live555
+	auto message = Nan::New<v8::String>("I'm a Node Hero!").ToLocalChecked();
+	info.GetReturnValue().Set(message);
+	
+	// AsyncQueueWorker(new NodeWorker(node_callback));
 }
 
 
